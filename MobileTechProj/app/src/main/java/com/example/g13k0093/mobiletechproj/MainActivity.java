fefc7a4057a01cf199219bb134776e6c07c54692
knoetzet <knoetzet@yahoo.com>
2016-05-18 11:00:18 +0200

    Removed junk

--- MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/MainActivity.java
+++ MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/MainActivity.java
@@ -13,50 +13,11 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final int PICK_CONTACT_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onClick(View view){
-        sendSMS();
-    }
-
-    public void onPress(View view){
-        pickcontact();
-    }
-
-    public void pickcontact() {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(intent, PICK_CONTACT_REQUEST);
-        }
-    }
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (resultCode == Activity.RESULT_OK && requestCode == PICK_CONTACT_REQUEST){
-            Uri contactData = data.getData();
-
-            Cursor c = getContentResolver().query(contactData, null, null, null, null);
-
-            if(c.moveToFirst()){
-                String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                Toast.makeText(this,"successfully picked a contact:" + name, Toast.LENGTH_LONG).show();
-            }
-        }
-
-    }
-
-
-    public void sendSMS(){
-        Intent send = new Intent(Intent.ACTION_VIEW);
-        send.setData(Uri.parse("http://www.google.com"));
-        send.putExtra("sms_body","Hi there!");
-
-        if(send.resolveActivity(getPackageManager())!=null)
-        {
-            startActivity(send);
-        }
     }
 }
 
