d6c5d6267fbbe1abcd1a6910568209e3a3c109c8
Nic <cription211@gmail.com>
2016-05-23 13:01:33 +0200

    camera permissions

--- MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/CameraActivity.java
+++ MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/CameraActivity.java
@@ -1,8 +1,11 @@
 package com.example.g13k0093.mobiletechproj;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,10 +19,11 @@ import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback  {
 
-    Camera camera;
+    static Camera camera = null;
     SurfaceView surfaceView;
     SurfaceHolder holder;
     String tag;
+    Camera.PictureCallback rawCallback;
 
     SurfaceHolder surfaceHolder;
 
@@ -28,35 +32,34 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+
+
+        surfaceView = (SurfaceView)findViewById(R.id.surfaceView1);
         holder = surfaceView.getHolder();
         holder.addCallback(this);
-        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         startcamera();
+
     }
 
 
-    private void startcamera(){
-        try{
-            camera = Camera.open();
-        }catch(RuntimeException error){
 
-            Log.e(tag, "init_camera: " + error);
-            return;
-            //you have failed or send to place to end camera
-        }
-        Camera.Parameters params;
-        params = camera.getParameters();
-        params.setPreviewFrameRate(20);
-        params.setPreviewSize(176, 144);
-        camera.setParameters(params);
+
+
+
+
+    private void startcamera(){
 
         try {
+            camera = Camera.open();
+            Camera.Parameters parameters = camera.getParameters();
+            //parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(parameters);
             camera.setPreviewDisplay(holder);
             camera.startPreview();
         }catch(Exception error){
-
-
+            Log.e(tag, "start_camera: " + error);
+            return;
         }
 
 
@@ -74,5 +77,11 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
         // TODO Auto-generated method stub
     }
 
+
+    public void onCaptureClick(View view){
+
+        Intent cap = new Intent(this, CapturedActivity.class);
+        startActivity(cap);
+    }
 
 }
--- MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/LoginActivity.java
+++ MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.g13k0093.mobiletechproj;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     String regUrl = "http://vmus.adu.org.za/dt_registration.php";
     Intent reg = new Intent(Intent.ACTION_VIEW, Uri.parse(regUrl));
+    private static final int REQUEST_CAMERARESULT = 201;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +24,51 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void onRegClick(View view){
+    public void onRegClick(View view) {
         startActivity(reg);
     }
-    
 
 
+    public void onLoginClick(View view) {
 
-    public void onLoginClick(View view){
-    // need to check register validity
-        Intent log = new Intent(this, CameraActivity.class);
-        startActivity(log);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Intent log = new Intent(this, CameraActivity.class);
+                startActivity(log);
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)) {
+                    Toast.makeText(this.getApplicationContext(), "Your Permission is needed to get access the camera", Toast.LENGTH_LONG).show();
+                }
+                requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, REQUEST_CAMERARESULT);
+            }
+        } else {
+            Intent log = new Intent(this, CameraActivity.class);
+            startActivity(log);
+        }
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERARESULT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
