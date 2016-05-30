e887770750a42e76a147fdcc40a727f46d8d3782
Nic <cription211@gmail.com>
2016-05-30 10:13:07 +0200

    Login Done

--- MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/LoginActivity.java
+++ MobileTechProj/app/src/main/java/com/example/g13k0093/mobiletechproj/LoginActivity.java
@@ -1,5 +1,11 @@
 package com.example.g13k0093.mobiletechproj;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
+
+
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Intent;
@@ -17,7 +23,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity  {
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import cz.msebera.android.httpclient.Header;
+
+public class LoginActivity extends AppCompatActivity {
+    String email;
+    String ADUid;
+    String password;
+    String API_key;
 
 
     String regUrl = "http://vmus.adu.org.za/dt_registration.php";
@@ -26,10 +55,15 @@ public class LoginActivity extends AppCompatActivity  {
     final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
 
+    String preferencefile = "com.biomapp.useraccount.1";
+
+    private SharedPreferences preferencesuser;
+    private SharedPreferences.Editor preferenceEditor;
+    public static final String myBioMappPREFERENCES = "BioMappUser";
+    private static final int PREFERENCE_MODE_PRIVATE =0;
 
 
 
-    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +78,37 @@ public class LoginActivity extends AppCompatActivity  {
         } else {
 
         }
+        preferencesuser = getSharedPreferences(myBioMappPREFERENCES,Context.MODE_PRIVATE );
 
+        //check if user has logged in before.
+            Check();
 
 
     }
+
+
+    public void Check(){
+
+       Toast.makeText(getApplicationContext(), "im checking", Toast.LENGTH_LONG).show();
+
+        if(preferencesuser != null) {
+            String name = preferencesuser.getString("userid", "none");
+
+            if (name != "none") {
+                Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+                Intent rec = new Intent(getApplicationContext(), RecordsActivity.class);
+                startActivity(rec);
+            }
+        }
+
+    }
+
+
+
 
     public void onRegClick(View view) {
         startActivity(reg);
     }
-
-
-
 
 
     @TargetApi(23)
@@ -63,7 +117,7 @@ public class LoginActivity extends AppCompatActivity  {
 
 
         if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
 
             return;
         }
@@ -85,7 +139,6 @@ public class LoginActivity extends AppCompatActivity  {
     public void onLoginClick(View view) {
 
 
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (this.getApplicationContext().checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 Intent log = new Intent(this, CameraActivity.class);
@@ -104,36 +157,122 @@ public class LoginActivity extends AppCompatActivity  {
     }
 
 
-        public void Camera(View view){
-
-            // json parsing extra
-            String email = ((EditText)findViewById(R.id.editText2)).getText().toString();
-            String ADUid = ((EditText)findViewById(R.id.editText)).getText().toString();
-            String password = ((EditText)findViewById(R.id.editText3)).getText().toString(); //encrypted
-
-            String http = "http://api.adu.org.za/validation/user/login?API_KEY=45d8dc528fc799f6d154497a6fe1f272&userid=90000&email=bobbytwoshoes@anemailserver.com&passid=a9c4cef5735770e657b7c25b9dcb807b";
-                    //"http://api.adu.org.za/validation/user/login?API_KEY=" + "&userid=" + ADUid + "&email=" + email + "&passid=" + password;
+    public String MD5Hash(String s) {
+        StringBuffer MD5Hash = new StringBuffer();
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
 
 
-            Intent log = new Intent(Intent.ACTION_VIEW, Uri.parse(http));
-
-            //example ---- http://api.adu.org.za/validation/user/login?API_KEY=45d8dc528fc799f6d154497a6fe1f272&userid=90000&email=bobbytwoshoes@anemailserver.com&passid=a9c4cef5735770e657b7c25b9dcb807b
-
-
-           /* if(return is success) {
-
-                    Save details and go to....
-
-
-                Intent rec = new Intent(this, RecordsActivity.class);
-                startActivity(rec);
-            }else
+            for (int i = 0; i < messageDigest.length; i++)
             {
-                Toast wrong login details...
-
-            }*/
+                String h = Integer.toHexString(0xFF & messageDigest[i]);
+                while (h.length() < 2)
+                    h = "0" + h;
+                MD5Hash.append(h);
+            }
         }
-
-
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return MD5Hash.toString();
     }
+
+
+    public void Camera(View view) {
+
+
+
+       email =   "cription211@gmail.com"    ;    //((EditText) findViewById(R.id.editText2)).getText().toString();
+        ADUid =  "16869";            //((EditText) findViewById(R.id.editText)).getText().toString();
+       password =  "887f113a8e1250020959eda260f7318c";  //MD5Hash(((EditText) findViewById(R.id.editText3)).getText().toString());
+       API_key = "f10dd19acf0acb4c0d16e9cfb2ad9d65";
+
+       // "http://api.adu.org.za/validation/user/login?API_KEY=45d8dc528fc799f6d154497a6fe1f272&userid=90000&email=bobbytwoshoes@anemailserver.com&passid=a9c4cef5735770e657b7c25b9dcb807b";
+
+        RequestParams params = new RequestParams();
+
+        params.put("userid",ADUid);
+        params.put("email",email);
+        params.put("passid",password);
+        params.put("API_KEY",API_key);
+
+
+        //"http://api.adu.org.za/validation/user/login?API_KEY=" + "&userid=" + ADUid + "&email=" + email + "&passid=" + password;
+        invokeWS(params);
+      //  Check();
+    }
+
+
+    public void invokeWS(RequestParams params){
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://api.adu.org.za/validation/user/login?",params ,new JsonHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject obj) {
+                //  super.onSuccess(statusCode, headers, response);
+                try {
+                  // Toast.makeText(getApplicationContext(), obj.getJSONObject("registered").getJSONObject("status").get("result").toString(), Toast.LENGTH_LONG).show();
+                  boolean permissions = obj.getJSONObject("registered").getJSONObject("status").get("result").equals("success");
+
+                        if(permissions){
+
+                            //get token
+                            String token = "" + obj.getJSONObject("registered").getJSONObject("status").get("token");
+                            //String email = "" + obj.getJSONObject("registered").getJSONObject("data").get("Email");
+
+
+                            preferenceEditor = preferencesuser.edit();
+
+                            preferenceEditor.putString("email",email);
+                            preferenceEditor.putString("password",password);
+                            preferenceEditor.putString("userid",ADUid);
+                            preferenceEditor.putString("token",token);
+                            preferenceEditor.commit();
+
+                            Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_LONG).show();
+
+
+                          //  NavigatetoHome();
+
+                        }
+                   else{
+                            //error not correct details
+                           Toast.makeText(getApplicationContext(), "invalid credentials", Toast.LENGTH_LONG).show();
+                       }
+
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+        });
+    }
+
+
+
+
+    public void NavigatetoHome()
+    {
+        Intent settingsIntent = new Intent(this, RecordsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears the stack so back will close the app.
+        startActivity(settingsIntent);
+     //   Intent rec = new Intent(this.getApplicationContext(),CameraActivity.class);
+     //   startActivity(rec);
+    }
+}
+
+
+
+
+
+
 
